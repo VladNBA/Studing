efcb6874c9dbf0fb558185fa750cf5b2057e4a1c
VladNBA <gvd22022006@gmail.com>
2024-10-14 14:10:07 +0700

    Add files via upload

---
+++ lab3.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main (){
+         char op;
+         float temp;
+         printf("vvedi temeraturu ");
+         scanf("%f%c" ,&temp,&op);
+         if (op == 'c' || op == 'C' ){
+                 temp=(temp*9/5)+32;
+                 printf("temperatura v forengate: %f\n", temp);
+
+         }
+         else if(op == 'f' || op == 'F' ){
+                 temp=(temp-32)*5/9;
+                 printf("temperatura v celsii: %f\n", temp);
+         }
+         else{
+                 printf("Error\n");
+         }
+}      
