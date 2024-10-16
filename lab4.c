bbfedca4d2b3a0ec11830da7bc98152866a3b964
VladNBA <gvd22022006@gmail.com>
2024-10-16 23:01:33 +0700

    Add files via upload

--- lab4.c
+++ lab4.c
@@ -1,36 +1,16 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 int main() {
-int num = 0;
-for ((int i = 0; i < n; i++)) {
-  for ((int j = 0; j < n; j++)) {
-    echo  "$num "// printf()
-    num=num + 1;
-  }
-  echo
+
+    int a = 6;
+        for (int i = 0; i <  8; i++){
+                printf(" ");
+                for ( int n = a; n > a-i; n--){
+                        printf(" ");
+                }
+                for ( int j = 0; j < a-i; j++){
+                        printf("$");
+                }
+                printf("\n");
+        }
 }
-}
-
-/*#!/bin/bash 
-
-
-read -p "3 x 3:" 
-
-if ! [[ "$n" =~ ^[0-9]+$ ]] || [ "$n" -le 0 ];
-  echo "9"
-  exit "1"
- fi
-  
-
-
-num=1
-
-for ((i = 0; i < n; i++));  
-  for ((j = 0; j < n; j++));   
-    echo  "$num "  
-    num=$((num + 1))  
-  done
-  echo  
-done
-
-*/
